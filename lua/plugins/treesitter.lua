2ddfa2533be219038b6e90a2271775c10d02c6f2
Aviral <kumaraviral100@gmail.com>
2024-02-27 18:25:59 +0530

    added vimdoc parser in tree-sitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,13 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local configs = require("nvim-treesitter.configs")
-        configs.compilers = { "gcc" }
-        configs.setup({
-            ensure_installed = { "lua", "cpp", "python", "javascript", "vim", "cmake" },
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.compilers = { "gcc" }
+		configs.setup({
+			ensure_installed = { "c", "cmake", "cpp", "javascript", "lua", "python", "vim", "vimdoc"  },
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
