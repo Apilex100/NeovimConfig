94f2ae4c56a763162dddaeec8882cb4a0fac937a
Aviral <kumaraviral100@gmail.com>
2024-02-27 17:03:24 +0530

    added tabwidth = 4, and formated all code

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,44 +1,44 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver", "jedi_language_server", "clangd" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "tsserver", "jedi_language_server", "clangd" },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-				cmd = { "lua-language-server.cmd" },
-			})
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-				cmd = { "typescript-language-server.cmd" },
-			})
-			lspconfig.jedi_language_server.setup({
-				capabilities = capabilities,
-				cmd = { "jedi-language-server.cmd" },
-			})
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-				cmd = { "clangd.cmd" },
-			})
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+                cmd = { "lua-language-server.cmd", "--stdio" },
+            })
+            lspconfig.tsserver.setup({
+                capabilities = capabilities,
+                cmd = { "typescript-language-server.cmd", "--stdio" },
+            })
+            lspconfig.jedi_language_server.setup({
+                capabilities = capabilities,
+                cmd = { "jedi-language-server.cmd", "--stdio" },
+            })
+            lspconfig.clangd.setup({
+                capabilities = capabilities,
+                cmd = { "clangd.cmd", "--stdio" },
+            })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
---
+++ lua/set.lua
@@ -0,0 +1,24 @@
+vim.g.mapleader = " "
+
+vim.opt.guicursor = "n-v-i-c:block-Cursor"
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 50
