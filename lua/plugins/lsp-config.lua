bf65cd240adac9381205cd16934b1e239be29ee0
Aviral <kumaraviral100@gmail.com>
2024-03-12 22:50:28 +0530

    added support for rust

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,44 +1,56 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "tsserver", "jedi_language_server", "clangd" },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "tsserver", "jedi_language_server", "clangd", "rust_analyzer" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-                cmd = { "lua-language-server.cmd", "--stdio" },
-            })
-            lspconfig.tsserver.setup({
-                capabilities = capabilities,
-                cmd = { "typescript-language-server.cmd", "--stdio" },
-            })
-            lspconfig.jedi_language_server.setup({
-                capabilities = capabilities,
-                cmd = { "jedi-language-server.cmd" },
-            })
-            lspconfig.clangd.setup({
-                capabilities = capabilities,
-                cmd = { "clangd.cmd", "--stdio" },
-            })
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				cmd = { "lua-language-server.cmd", "--stdio" },
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+				cmd = { "typescript-language-server.cmd", "--stdio" },
+			})
+			lspconfig.jedi_language_server.setup({
+				capabilities = capabilities,
+				cmd = { "jedi-language-server.cmd" },
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+				cmd = { "clangd.cmd", "--stdio" },
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+				cmd = { "rust-analyzer.cmd" },
+				filetypes = { "rust" },
+				settings = {
+					["rust-analyzer"] = {
+						diagnostics = {
+							enable = false,
+						},
+					},
+				},
+			})
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
